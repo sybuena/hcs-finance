8b2f75a10d23c8204b3c7a04e50b8d4767113656
Christian Buenavista <Christian@propertyinabox.com.au>
2014-07-17 00:58:41 +0800

    localhost special case for anjo

--- application/config/config.php
+++ application/config/config.php
@@ -14,8 +14,13 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://'.$_SERVER['HTTP_HOST'];
+//special case for anjo
 
+if($_SERVER['HTTP_HOST'] == 'localhost') {
+	$config['base_url']	= 'http://'.$_SERVER['HTTP_HOST'].'/git/hcs-finance/';	
+} else {
+	$config['base_url']	= 'http://'.$_SERVER['HTTP_HOST'];
+}
 /*
 |--------------------------------------------------------------------------
 | Index File
