8246b1101674a0e9a4029857daa4cb5bd5933d60
AnjoTamondong <AnjoTamondong@github.com>
2014-07-17 02:50:10 -0700

    Log view

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -4,7 +4,7 @@
 	
 		<div class="wrapper">
 		
-			<img src="/assets/img/logo.png" id="login-logo"/>
+			<img src="<?php echo base_url().'assets/img/logo.png' ?>" id="login-logo"/>
 		
 			<!-- Box -->
 			<div class="widget widget-heading-simple widget-body-gray">
@@ -14,18 +14,18 @@
 					
 					<div>
 						<label>Username or Email</label>
-						<input type="text" class="input-block-level form-control" placeholder="Your Username or Email address"> 
+						<input type="text" id="username" class="input-block-level form-control" placeholder="Your Username or Email address"> 
 						<label>Password</label>
-						<a class="pull-right" href="">forgot it?</a>
 						<div class="clearfix"></div>
-						<input type="password" class="input-block-level form-control margin-none" placeholder="Your Password">
+						<input type="password" id="password" class="input-block-level form-control margin-none" placeholder="Your Password">
+						<a class="pull-right" href="">forgot it?</a>
 						<div class="separator bottom"></div> 
 						<div class="row">
 							<div class="col-md-8">
 								
 							</div>
 							<div class="col-md-4 center">
-								<button class="btn btn-block btn-red" type="submit">Sign in</button>
+								<button class="btn btn-block btn-red" type="submit" id="submit">Sign in</button>
 							</div>
 						</div>
 					</div>
@@ -33,7 +33,7 @@
 							
 				</div>
 				<div class="widget-footer">
-					<p class="glyphicons restart"><i></i>Please enter your username and password ...</p>
+					<p class="glyphicons restart center" id="error"><i></i>Fill-up the required fields.</p>
 				</div>
 			</div>
 			<!-- // Box END -->
@@ -44,3 +44,39 @@
 	
 </div>
 
+<script type="text/javascript">
+	// log in validation
+	$(document).ready(function() {
+		$('#submit').click(function(){			
+		var username = $('#username').val();
+		var password = $('#password').val();
+
+			// login conditions
+			if(username.length == 0 && password.length == 0){
+				$('#error').text('Please enter your Username and Password.');
+			}
+			else if(username.length == 0){
+				$('#error').text('Please enter your Username/Email addres');		}
+			else if(password.length == 0){
+				$('#error').text('Please enter your Password');				
+			}
+			else if(password.length < 6){
+				$('#error').text('Invalid password ( MINIMUM of 5 characters )');						
+			}
+			else{				
+				$.ajax({
+			         type: 'POST',
+			         url: '<?php base_url(). "application/controllers/login.php" ?>', 
+			         data: {logUsername: username, logPassword: password},
+			         dataType: 'text',  
+			         cache:false,
+			         success: 
+			              function(data){
+			                alert('Success');  //as a debugging message.
+			              }
+			          });
+			}
+		})
+	});
+
+</script>
