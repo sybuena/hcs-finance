2bc39e53a68653b40f84d8ff2e06be520dd29c5e
AnjoTamondong <AnjoTamondong@github.com>
2014-07-18 05:52:31 -0700

    my_controller

--- application/core/My_Controller.php
+++ application/core/My_Controller.php
@@ -7,15 +7,37 @@ class MY_Controller extends CI_Controller {
 	
 	public function __construct() { 
 		parent::__construct();
-		
-		
- 		$this->load->helper('url');
-		
+		//chec if there is user on the session
+		$this->_user  = $this->session->userdata('login_user');
 
-
+		//print_r($this->_user);exit;
 	}
 	
 	public function index() {
 	}
+
+	protected function _throwSuccess($message = null) {
+
+		if(is_null($message)) {
+			$message = 'Success';
+		}
+
+		echo json_encode(array(
+			'error' => 0,
+			'message' => $message
+		));
+	}
 	
+	protected function _throwError($message = null){
+
+		if(is_null($message)) {
+			$message = 'Error';
+		}
+
+		echo json_encode(array(
+			'error' => 1,
+			'message' => $message
+		));
+
+	}
 }
