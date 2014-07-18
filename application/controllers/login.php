b40a19825e530d92b5ce1bf645b7ba94dc3b2fb2
AnjoTamondong <AnjoTamondong@github.com>
2014-07-18 05:50:35 -0700

    login.php

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,16 +1,39 @@
 <?php
 
-class Login extends CI_Controller {
+class Login extends MY_Controller {
+
+	const USERNAME = 'test';
+	const PASSWORD = 'test';
 
     /**
      * Index page 
      */
     public function index(){
 
-        echo 'Success';
-
     }
 
+    public function check() {
+    	$username = $_POST['logUsername'];
+    	$password = $_POST['logPassword'];
+
+		if($username == self::USERNAME && $password == self::PASSWORD) {
+			$sessionData = array(
+				'username' => $username,
+				'password' => $password
+				);
+
+			$this->session->set_userdata('login_user', $sessionData);
+
+			$this->_throwSuccess();
+		} else {
+			$this->_throwError();
+		} 	
+		exit;
+    }
+
+    public function now() {
+    	echo 'now';
+    }
 }
 
 ?>
