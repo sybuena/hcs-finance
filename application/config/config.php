cd57996c13829f3751960e26154542af8fb11312
AnjoTamondong <AnjoTamondong@github.com>
2014-07-18 05:51:07 -0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -49,7 +49,7 @@ $config['index_page'] = 'index.php';
 | 'ORIG_PATH_INFO'	Uses the ORIG_PATH_INFO
 |
 */
-$config['uri_protocol']	= 'AUTO';
+$config['uri_protocol']	= 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
@@ -229,7 +229,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '@nJ0';
 
 /*
 |--------------------------------------------------------------------------
