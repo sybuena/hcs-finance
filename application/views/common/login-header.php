8164877e22263a92b8a7adc244f55ae794ef01c5
AnjoTamondong <AnjoTamondong@github.com>
2014-07-19 04:43:50 -0700

    login-header

--- application/views/common/login-header.php
+++ application/views/common/login-header.php
@@ -1,7 +1,8 @@
+<!DOCTYPE html>
 <html class="front fluid top-full menuh-top sticky-top js flexbox flexboxlegacy no-touch rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent"><script id="tinyhippos-injected">if (window.top.ripple) { window.top.ripple("bootstrap").inject(window, document); }</script><!-- <![endif]-->
 	<head>
-		<title>HCS finance</title>
-		
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>HCS finance</title>		
 		<!-- Meta -->
 		<meta charset="utf-8">
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,13 +17,15 @@
 	      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 	    <![endif]-->
 
-	    <script src="<?php echo base_url().'/assets/js/jquery.min.js'; ?>"></script>
-		<script src="<?php echo base_url().'/assets/js/jquery-migrate.min.js'; ?>"></script>
-		<script src="<?php echo base_url().'/assets/js/modernizr.js'; ?>"></script>  
-		<script src="<?php echo base_url().'/assets/js/less.min.js'; ?>"></script>
-		<script src="<?php echo base_url().'/assets/js/excanvas.js'; ?>"></script>
-		<script src="<?php echo base_url().'/assets/js/ie.prototype.polyfill.js'; ?>"></script>
-		<script>if (/*@cc_on!@*/false && document.documentMode === 10) { document.documentElement.className+=' ie ie10'; }</script>
+	    <script type="text/javascript" src="<?php echo base_url().'/assets/js/jquery.min.js'; ?>"></script>
+		<script type="text/javascript" src="<?php echo base_url().'/assets/js/jquery-migrate.min.js'; ?>"></script>
+		<script type="text/javascript" src="<?php echo base_url().'/assets/js/modernizr.js'; ?>"></script>  
+		<script src="/assets/js/ie.prototype.polyfill.js"></script>		
+		<script>
+			if (/*@cc_on!@*/false && document.documentMode === 10) { 
+				document.documentElement.className+=' ie ie10'; 
+			}
+		</script>
 
 	</head>
 	
@@ -32,8 +35,7 @@
 	<div class="container-fluid">
 		
 		<!-- Content -->
-		<div id="content" style="background-image: url(/assets/img/pattern.png);background-position: center 0px;
-background-repeat: no-repeat;">
+		<div id="content">
 		
 			<!-- Top navbar (note: add class "navbar-hidden" to close the navbar by default) -->
 			<div class="navbar main hidden-print">
