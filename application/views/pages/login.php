b207b0d80eaad0f1290143c03368ffcc75ea6edc
AnjoTamondong <AnjoTamondong@github.com>
2014-07-18 23:52:40 -0700

    log in

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -48,21 +48,35 @@
 	// run submit when enter is triggered
 	// red the textbox and text when error
 	// log in validation
-	$(document).ready(function() {
-		$('#submit').click(function(){			
+	
+	// When enter is pressed
+	$('input.required').keypress(function(event) {
+	    if (event.which == 13) {
+	    	login();
+	    }
+	});
+
+	// When submit button in clicked
+	$('button#submit').click(function(){
+		login();
+	});
+
+	// Login Validation
+	function login(){			
 		
 			var username 	= $('input#username').val();
 			var password 	= $('input#password').val();
 			var error    	= 0;
 			var message 	= '';
+			var red 		= '#c94437'
 
-			if(username.length == 0 && password.length == 0){
+			if(username == '' && password == ''){
 				message = 'Please enter your Username and Password';
 				error = 1;
-			} else if(username.length == 0){
+			} else if(username == ''){
 				message = 'Please enter your Username/Email addres';	
 				error = 1;	
-			} else if(password.length == 0){
+			} else if(password == ''){
 				message = 'Please enter your Password';	
 				error = 1;			
 			} else if(password.length < 4){
@@ -72,18 +86,22 @@
 
 			// if there is an error 
 			if(error) {
-				$('#error').text(message);	
+				$('#error').text(message).css({
+		                    "color": red
+		                });	
+
 
 				// username textbox
 				$('input[type="text"].required').each(function() {
+					// if the username textbox is empty
 		            if ($.trim($(this).val()) == '') {
 		                $(this).css({
-		                    "border": "1px solid red"
+		                    "border-color": red
 		                });
 		                $('label#username').css({
-		                    "color": "red"
+		                    "color": red
 		                });
-
+		             // else its filled up
 		            } else {
 		                $(this).css({
 		                    "border": ""
@@ -94,25 +112,25 @@
 		            }
 		        });		
 				
-
 				// password textbox
 				$('input[type="password"].required').each(function() {
+					// if password textbox is empty
 		            if ($.trim($(this).val()) == '') {
 		                $(this).css({
-		                    "border": "1px solid red"
+		                    "border-color": red
 		                });
 		                $('label#password').css({
-		                    "color": "red"
+		                    "color": red
 		                });
-		                		                
+		            // else if the password.length is < 4    		                
 		            } else if ($.trim($(this).val()).length < 4) {
 		                $(this).css({
-		                    "border": "1px solid red"
+		                    "border-color": red
 		                });
 		                $('label#password').css({
-		                    "color": "red"
+		                    "color": red
 		                });
-		                		                
+		            // else its filled-up    		                
 		            } else {
 		                $(this).css({
 		                    "border": ""
@@ -134,18 +152,34 @@
 			         success 	: function(data){
 			             
 			             if(data.error) {
-			             	$('#error').text('Username and password is not registered');	
+			             	// When both field is empty validate red text
+			             	$('#error').text('Username and password is not registered').css({
+			             			"color": red
+			             	});	
+			             	// When both is empty - red borders
+							$('input.required').each(function() {
+					                $(this).css({
+					                    "border-color": red
+					                });
+					                $('label#username').css({
+					                    "color": red
+					                });
+					                $('label#password').css({
+					                    "color": red
+					                });
+					        });		
+				
 			             } else {
 			             	location.reload();
 			             }
 		             },
+
 		             error 		: function(error) {
 		             	
 		             }
 		          });
 			}
 			
-		})
-	});
+		};
 
 </script>
