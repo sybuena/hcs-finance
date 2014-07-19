a91972534a5761a4768393e1f40f49ebc81e72d5
AnjoTamondong <AnjoTamondong@github.com>
2014-07-19 05:12:52 -0700

    all stuff

---
+++ application/controllers/check.php
@@ -0,0 +1,10 @@
+<?php
+
+Class Check extends CI_Controller {
+
+	public function index(){
+		echo "check page";
+	}	
+}
+
+?>
---
+++ application/controllers/web/user.php
@@ -0,0 +1,20 @@
+<?php
+
+class User extends CI_Controller {
+
+    /**
+     * Index page 
+     */
+    public function index(){
+        echo 'inside folder';
+    }
+
+    public function yo($var){
+        echo $var;
+    }
+
+
+
+}
+
+?>
